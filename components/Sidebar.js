5303de4f1747d7de3dd582049eafaf552bd4acc0
iamajraj <akmalraj07@gmail.com>
2022-08-02 22:04:09 +0600

    news section of widgets created

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,58 +1,58 @@
 /* eslint-disable @next/next/no-img-element */
-import Image from 'next/image';
-import SidebarMenuItem from './SidebarMenuItem';
-import { HomeIcon } from '@heroicons/react/solid';
+import Image from "next/image";
+import SidebarMenuItem from "./SidebarMenuItem";
+import { HomeIcon } from "@heroicons/react/solid";
 import {
-  HashtagIcon,
-  BellIcon,
-  InboxIcon,
-  DotsCircleHorizontalIcon,
-  BookmarkIcon,
-  ClipboardIcon,
-  UserIcon,
-  DotsHorizontalIcon,
-} from '@heroicons/react/outline';
+    HashtagIcon,
+    BellIcon,
+    InboxIcon,
+    DotsCircleHorizontalIcon,
+    BookmarkIcon,
+    ClipboardIcon,
+    UserIcon,
+    DotsHorizontalIcon,
+} from "@heroicons/react/outline";
 
 export default function Sidebar() {
-  return (
-    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
-      {/* Twitter Logo */}
-      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
-        <Image
-          width="50"
-          height="50"
-          src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
-          alt="logo"
-        />
-      </div>
-      {/* Menu */}
-      <div className="mt-4 mb-2.5 xl:items-start">
-        <SidebarMenuItem active text="Home" Icon={HomeIcon} />
-        <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notifications" Icon={BellIcon} />
-        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-        <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-        <SidebarMenuItem text="Profile" Icon={UserIcon} />
-        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
-      </div>
-      {/* Button */}
-      <button className="bg-blue-400 text-white w-56 rounded-full h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-        Tweet
-      </button>
-      {/* Mini-Profile */}
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img
-          src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
-          alt="user profile"
-          className="h-10 w-10 rounded-full object-cover xl:mr-2"
-        />
-        <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">William Johnson</h4>
-          <p className="text-gray-500">@willijohn</p>
+    return (
+        <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
+            {/* Twitter Logo */}
+            <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
+                <Image
+                    width="50"
+                    height="50"
+                    src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
+                    alt="logo"
+                />
+            </div>
+            {/* Menu */}
+            <div className="mt-4 mb-2.5 xl:items-start">
+                <SidebarMenuItem active text="Home" Icon={HomeIcon} />
+                <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
+                <SidebarMenuItem text="Notifications" Icon={BellIcon} />
+                <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+                <SidebarMenuItem text="Bookmark" Icon={BookmarkIcon} />
+                <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+                <SidebarMenuItem text="Profile" Icon={UserIcon} />
+                <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+            </div>
+            {/* Button */}
+            <button className="bg-blue-400 text-white w-56 rounded-full h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+                Tweet
+            </button>
+            {/* Mini-Profile */}
+            <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+                <img
+                    src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg"
+                    alt="user profile"
+                    className="h-10 w-10 rounded-full object-cover xl:mr-2"
+                />
+                <div className="leading-5 hidden xl:inline">
+                    <h4 className="font-bold">William Johnson</h4>
+                    <p className="text-gray-500">@willijohn</p>
+                </div>
+                <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
+            </div>
         </div>
-        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
-      </div>
-    </div>
-  );
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
 
-export default function Home() {
+export default function Home({ newsResults }) {
     return (
         <div>
             <Head>
@@ -23,10 +23,26 @@ export default function Home() {
                 <Feed />
 
                 {/* Widgets */}
-                <Widgets />
+                <Widgets newsResults={newsResults} />
 
                 {/* Modal */}
             </main>
         </div>
     );
+}
+
+// https://newsapi.org/v2/top-headlines?sources=mashable&apiKey=f0df678708d74cafb2d72fc9882ee497
+
+export async function getServerSideProps() {
+    const newsResults = await (
+        await fetch(
+            "https://newsapi.org/v2/top-headlines?sources=mashable&apiKey=f0df678708d74cafb2d72fc9882ee497"
+        )
+    ).json();
+
+    return {
+        props: {
+            newsResults: newsResults.articles,
+        },
+    };
 }
